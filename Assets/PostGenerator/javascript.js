f9e2cc8660c81b0fe804636ae36cae986a5f2696
aqilmujahidiin <aqilmujahidiin@gmail.com>
2024-07-31 10:18:15 +0700

    Update javascript.js

--- Assets/PostGenerator/javascript.js
+++ Assets/PostGenerator/javascript.js
@@ -23,7 +23,7 @@ function addPackage() {
         <label for="package${packageCount}">Package ${packageCount}:</label>
         <input type="text" id="package${packageCount}Description" name="package${packageCount}Description" placeholder="Package description" class="input-full-width">
         <input type="number" id="price${packageCount}" name="price${packageCount}" placeholder="Price (USD)" class="input-half-width">
-        <button type="button" onclick="removePackage(${packageCount})">Remove Package</button>
+        <button class="gohapus"type="button" onclick="removePackage(${packageCount})">x</button>
     `;
     packageOptions.appendChild(packageDiv);
 }
